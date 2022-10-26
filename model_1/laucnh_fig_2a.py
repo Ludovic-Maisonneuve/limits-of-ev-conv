2dd22d95354d03421237a59fefbffb2786634d2b
Ludovic-Maisonneuve <66014130+Ludovic-Maisonneuve@users.noreply.github.com>
2022-10-26 15:25:13 +0200

    Update laucnh_fig_2a.py

--- model_1/laucnh_fig_2a.py
+++ model_1/laucnh_fig_2a.py
@@ -1,78 +1,50 @@
 import numpy as np
-import os
 
 eps = 0.01
+tp = 1
+l = 0.1
+lp = 0.1
+N = 10
+Np = 20
+d = 2 * eps
+cri = 0.1 * eps
+to = 0
 
-a1 = 1
-a2 = 1
-b = 1
-l1 = 0.1
-l2 = 0.1
-N1 = 10
-N2 = 10
-d = 5 * eps
-cri1 = 0 * eps
-cri2 = cri1
-s1 = 0.5 * eps
-s2 = 0.5 * eps
-to1 = 0
-to2 = 1
+L_cri = np.linspace(0, 1 * eps, 100)
 
-def t1_t2_eq(a1, a2, b, l1, l2, N1, N2, d, cri1, cri2, s1, s2, to1, to2): #function returning the phenotypic distance between the two species at equilibrium
+def d_teq_tp_strong(a, b, tp, l, lp, N, Np, d, cri, s, to): #function returning the phenotypic distance between the two species at equilibrium
 
-    def beta(t1, p1, t2, p2): #function computing the selection vector
-        betat1 = - 2 * a1 * (t1 - p1) - 4 * b * (t1 - t2) * l2 * N2 * np.exp(- b * (t1 - t2) ** 2) * (
-                1 / (1 + l1 * N1 + l2 * N2 * np.exp(- b * (t1 - t2) ** 2) - d) - 1 / (
-                1 + l1 * N1 + l2 * N2 * np.exp(- b * (t1 - t2) ** 2))) - 4 * s1 * (t1 - to1)
-        betap1 = - 2 * a1 * (p1 - t1) + 2 * cri1 * a1 * (t1 - t2) / (
-                cri1 + np.exp(a1 * (t1 - t2) * (2 * p1 - t1 - t2)) * N1 / N2)
-        betat2 = - 2 * a2 * (t2 - p2) - 4 * b * (t2 - t1) * l1 * N1 * np.exp(- b * (t2 - t1) ** 2) * (
-                1 / (1 + l1 * N1 * np.exp(- b * (t1 - t2) ** 2) + l2 * N2 - d) - 1 / (
-                1 + l1 * N1 * np.exp(- b * (t1 - t2) ** 2) + l2 * N2)) - 4 * s2 * (t2 - to2)
-        betap2 = - 2 * a2 * (p2 - t2) + 2 * cri2 * a2 * (t2 - t1) / (
-                cri2 + np.exp(a2 * (t2 - t1) * (2 * p2 - t1 - t2)) * N2 / N1)
-        return np.array([betat1, betap1, betat2, betap2])
+    def beta(t, p): #function computing the selection vector
+        betat = - 2 * a * (t - p) - 4 * b * (t - tp) * lp * Np * np.exp(- b * (t - tp) ** 2) * (
+                1 / (1 + l * N + lp * Np * np.exp(- b * (t - tp) ** 2) - d) - 1 / (
+                1 + l * N + lp * Np * np.exp(- b * (t - tp) ** 2))) - 4 * s * (t - to)
+        betap = - 2 * a * (p - t) + 2 * cri * a * (t - tp) / (cri + np.exp(a * (t - tp) * (2 * p - t - tp)) * N / Np)
+        return np.array([betat, betap])
 
-    X0 = np.array([to1, to1, to2, to2]) #vector with initial trait and preference
+    teq = to #set initial trait in species 1
+    peq = to #set initial preference in species 1
+
+    X0 = np.array([teq, peq]) #vector with initial trait and preference
     X = X0
-    G = 0.01 * np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]) #define matrix of genetic covariance
-    dX = np.array([1, 1, 1, 1])
+    G = 0.01 * np.array([[1, 0], [0, 1]]) #define matrix of genetic covariance
+    dX = np.array([1, 1])
     i = 0
-    while np.sqrt(dX[0] ** 2 + dX[1] ** 2 + dX[2] ** 2 + dX[3] ** 2) / 4 > 0.00000000000000001 and (
-            X[0] ** 2 < 10000000 or X[1] ** 2 < 10000000 or X[2] ** 2 < 10000000 or X[
-        3] ** 2 < 10000000) and i < 1000000:
-        i += 1
-        dX = np.dot(G, beta(X[0], X[1], X[2], X[3]))
+    while np.sqrt(dX[0]**2 + dX[1]**2) > 0.000000000000001 and X[0]**2 + X[1]**2 < 10000000: #while equilibrium is not reached
+        i+= 1
+        dX = np.dot(G, beta(X[0], X[1]))
         X = X + dX #evolutionary dynamics of the mean trait and preference values
     print(i)
+    if X[0]**2 + X[1]**2 < 10000000:
+        return np.abs(X[0] - tp)
+    else:
+        return float('inf')
 
-    return X[0], X[2]
+L_eps_ = [1, 0.1, 0.01]
 
-res = 100
-pas = 1 / res
-L_l1 = np.linspace(0 + pas, 1 - pas, res)
-pas = 25 / res
-L_N1 = np.linspace(0 + pas, 25 - pas, res)
-
-print('\n cri = 0 \n', flush=True)
-
-for i, N1 in enumerate(L_N1):
-    for j, l1 in enumerate(L_l1):
-        if i > 1000 and os.path.isfile('results/fig_4/cri0/' + str(j) + '_' + str(i) + '.npy') == False:
-            print(i, j, flush=True)
-            t1, t2 = t1_t2_eq(a1, a2, b, l1, l2, N1, N2, d, cri1, cri2, s1, s2, to1, to2)
-            np.save('results/fig_4/cri0/' + str(j) + '_' + str(i), [t1,t2])
-
-cri1 = 0.5 * eps
-cri2 = cri1
-
-print('\n cri = 0.0005 \n', flush=True)
-
-for i, N1 in enumerate(L_N1):
-    for j, l1 in enumerate(L_l1):
-        if i > 1000 and os.path.isfile('results/fig_4/cri5/' + str(j) + '_' + str(i) + '.npy') == False:
-            print(i, j, flush=True)
-            t1, t2 = t1_t2_eq(a1, a2, b, l1, l2, N1, N2, d, cri1, cri2, s1, s2, to1, to2)
-            np.save('results/fig_4/cri5/' + str(j) + '_' + str(i), [t1,t2])
-
-
+for eps_ in L_eps_:
+    print('eps_', eps_, flush=True)
+    a = 1 * eps_
+    b = 1 * eps_
+    s = 0.5 * eps * eps_
+    L = [d_teq_tp_strong(a, b, tp, l, lp, N, Np, d, cri, s, to) for cri in L_cri]
+    np.save('results/fig_2a/L_esp_'+str(eps_), L)
